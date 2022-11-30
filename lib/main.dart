fb4419bc49544a7be8787fe4f2a24dc732601045
erickdarmawan <erickdarmawan83@gmail.com>
2022-11-30 15:49:30 +0700

    finished it_box_tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'pertanyaan.dart';
-import 'jawaban.dart';
+import 'package:it_box_tutorial/quiz.dart';
 
-// void main(){
-//   runApp(MyApp());
-// }
+import 'hasil.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,54 +12,69 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _soalIndex = 0;
+  var totalSkor = 0;
+
   var pertanyaan = [
     {
       'Pertanyaan': 'Tempat - tempat favorit apa yang sering anda kunjungi?',
       'Jawaban': [
-        'Mall Taman Anggrek',
-        'Taman Mini Indonesia Indah',
-        'Mall Pondok Indah',
-        'Pantai Indah Kapuk'
+        {'teks': 'Pegunungan', 'skor': 10},
+        {'teks': 'Pantai', 'skor': 9},
+        {'teks': 'Mall', 'skor': 7},
+        {'teks': 'Restoran', 'skor': 1},
       ],
     },
     {
       'Pertanyaan': 'Apa favorit hobi anda?',
       'Jawaban': [
-        'Main komputer',
-        'Masak',
-        'Jalan - jalan',
+        {'teks': 'Main video game', 'skor': 10},
+        {'teks': 'Main ngoding', 'skor': 8},
+        {'teks': 'Main komputer', 'skor': 9},
+        {'teks': 'Jalan - jalan', 'skor': 10},
       ],
     },
     {
       'Pertanyaan': 'Apa pekerjaan anda?',
       'Jawaban': [
-        'IT Engineer',
-        'Mobile Developer',
+        {'teks': 'IT Engineer', 'skor': 8},
+        {'teks': 'Mobile Developer', 'skor': 9},
+        {'teks': 'Pemasak', 'skor': 8},
+        {'teks': 'Grafik Designer', 'skor': 9},
       ],
     },
   ];
 
-  void _jawaban() {
+  void resetQuiz() {
+    setState(() {
+      _soalIndex = 0;
+      totalSkor = 0;
+    });
+  }
+
+  void _jawaban(int skor) {
+    totalSkor = totalSkor + skor;
     setState(() {
       _soalIndex += 1;
     });
-    print(_soalIndex);
+    if (_soalIndex < pertanyaan.length) {
+      print('masih ada soal berikutnya');
+    } else {
+      print('sudah tidak anda soal');
+    }
   }
 
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Applikasi Kuis'),
-        ),
-        body: Column(children: [
-          Pertanyaan(pertanyaan[_soalIndex]['Pertanyaan']),
-          ...(pertanyaan[_soalIndex]['Jawaban'] as List<String>)
-              .map((jawabanText) {
-            return Jawaban(_jawaban, jawabanText);
-          }).toList(),
-        ]),
-      ),
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Applikasi Kuis'),
+            ),
+            body: _soalIndex < pertanyaan.length
+                ? Quiz(
+                    jawaban: _jawaban,
+                    pertanyaan: pertanyaan,
+                    soalIndex: _soalIndex)
+                : Hasil(totalSkor, resetQuiz)));
   }
 }
